35d92e18847f0a8676d8f0bcdc75336c4af16521
lullabybot <jeanputra61@gmail.com>
2024-09-11 07:17:35 +0700

    ppppppp

--- src/components/BestSeller.jsx
+++ src/components/BestSeller.jsx
@@ -3,11 +3,14 @@ import React from 'react'
 export const BestSeller = () => {
   return (
     <section>
-        <div className='m-[-100px] h-[100vh] bg-[#F9BFE0]'>
-        <div className='flex justify-center items-center'>
-            <h1 className='mt'>
-            BestSeller
-            </h1>
+        <div className='m-[-170px] h-[100vh] bg-[#F9BFE0]'>
+        <div className='flex justify-center items-center m-10'>
+            <div className='flex justify-center items-center '>
+                <div className='bg-white w-[60vh] h-[60vh] flex justify-center items-center'></div>
+                <h1 className=''>
+                BestSeller
+                </h1>
+            </div>
         </div>
     </div>
     </section>
